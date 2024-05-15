10ff9fcd7449bd3ae076dc1b7b94cc58fca675a0
Nikola Hristov <Nikola@PlayForm.Cloud>
2024-05-15 04:57:10 +0300

    squash!

--- cli/main.rs
+++
@@ -1,5 +0,0 @@
-extern crate deno_cli;
-
-fn main() {
-  deno_cli::main();
-}
--- std/examples/cat.ts
+++
@@ -1,7 +0,0 @@
-// Copyright 2018-2019 the Deno authors. All rights reserved. MIT license.
-const filenames = Deno.args.slice(1);
-for (const filename of filenames) {
-  const file = await Deno.open(filename);
-  await Deno.copy(Deno.stdout, file);
-  file.close();
-}
