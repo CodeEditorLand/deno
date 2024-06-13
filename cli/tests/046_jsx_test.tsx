490083da56271a7a04f37c43fc7cdf9c5ec92752
Nikola Hristov <Nikola@PlayForm.Cloud>
2024-06-13 18:30:21 +0300

    squash!

--- cli/tests/046_jsx_test.tsx
+++
@@ -1,9 +0,0 @@
-const React = {
-  createElement(factory: any, props: any, ...children: any[]) {
-    return {factory, props, children}
-  }
-}
-const View = () => (
-  <div class="deno">land</div>
-)
-console.log(<View />)
